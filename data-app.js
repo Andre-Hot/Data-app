fec6d359f87743ec2db9d823cbccb325423436aa
Andre-Hot <123978520+Andre-Hot@users.noreply.github.com>
2023-03-23 09:00:55 +0100

    just a few changes in main directory

---
+++ data-app.js
@@ -0,0 +1,40 @@
+const bellsprout = {
+
+    name: "Bellsprout",
+    description: "Is a grass/poison-type Pokemon",
+    ability: "Chlorophyll",
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/069.png",
+    footprint:undefined, 
+    dexindex: "#0069",
+    type: "Grass",
+    subtype: "Poison",
+    weaknesses: "Fire, Phsychic, Flying, Ice",
+    gender: "male, female", 
+    weight: "8.8 lbs",
+    height: "2'04",
+    generation: "1",
+    spilversion: "Pokemon",
+    canEvolve: "true",
+    statsHP: "3",
+    statsAttack: "5",
+    statsDefence: "3",
+    statsSpecialAttack: "5",
+    statsSpecialDefence: "2",
+    statsSpeed:"3",
+
+
+}
+
+function initApp() {}
+
+function fetchJSON() {}
+
+function showCharacters() {}
+
+function showCharacter() { }
+
+function showHouse() {}
+
+function calcAge() {}
+
+function getDescription(){}
