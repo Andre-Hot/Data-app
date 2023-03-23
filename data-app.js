5ea2aa6b149a8b3b6333ec0d9cce79bbbfa1c275
Andre-Hot <123978520+Andre-Hot@users.noreply.github.com>
2023-03-23 20:11:46 +0100

    still working on few changes

--- data-app.js
+++ data-app.js
@@ -1,37 +1,53 @@
-const bellsprout = {
+window.addEventListener("load", initApp);
 
-    name: "Bellsprout",
-    description: "Is a grass/poison-type Pokemon",
-    ability: "Chlorophyll",
-    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/069.png",
-    footprint:undefined, 
-    dexindex: "#0069",
-    type: "Grass",
-    subtype: "Poison",
-    weaknesses: "Fire, Phsychic, Flying, Ice",
-    gender: "male, female", 
-    weight: "8.8 lbs",
-    height: "2'04",
-    generation: "1",
-    spilversion: "Pokemon",
-    canEvolve: "true",
-    statsHP: "3",
-    statsAttack: "5",
-    statsDefence: "3",
-    statsSpecialAttack: "5",
-    statsSpecialDefence: "2",
-    statsSpeed:"3",
+async function initApp() {
+/*
+    const bellsprout = {
+
+        name: "Bellsprout",
+        description: "Is a grass/poison-type Pokemon",
+        ability: "Chlorophyll",
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/069.png",
+        footprint: undefined,
+        dexindex: "#0069",
+        type: "Grass",
+        subtype: "Poison",
+        weaknesses: "Fire, Phsychic, Flying, Ice",
+        gender: "male, female",
+        weight: "8.8 lbs",
+        height: "2'04",
+        generation: "1",
+        spilversion: "Pokemon",
+        canEvolve: "true",
+        statsHP: "3",
+        statsAttack: "5",
+        statsDefence: "3",
+        statsSpecialAttack: "5",
+        statsSpecialDefence: "2",
+        statsSpeed: "3",
 
 
+    }
+*/
+  
+
+    
 }
 
-function initApp() {}
 
-function fetchJSON() {}
 
-function showCharacters() {}
 
-function showCharacter() { }
+
+
+function showPokemon(pokemon) {
+    document.querySelector().insertAdjacentHTML("beforeend"
+    /*html*/
+    <h1>${pokemon.name}</h1>
+        <div id="picture">
+        <image src="${pokemon.image}"></image>
+         </div>
+    )
+ }
 
 function showHouse() {}
 
